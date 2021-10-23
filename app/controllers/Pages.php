e12731199027ad46b84a2475b7cf1ca239bd40fd
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-23 20:23:43 +0600

    Model created

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,7 +3,7 @@
 class Pages extends Controller {
 
     public function __construct() {
-
+        $this->userModel = $this->model('User');
     }
 
     public function index() {
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -25,4 +25,53 @@ class Database {
             echo $this->error;
         }
     }
+
+    // Allows us to write queries
+    public function query($sql) {
+        $this->statement = $this->dbHandler->prepare($sql);
+    }
+
+    // Bind values
+    public function bind($parameter, $value, $type = null) {
+        switch (is_null($type)) {
+            case is_int($value):
+                $type = PDO::PARAM_INT;
+                break;
+
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+
+            DEFAULT:
+                $type = PDO::PARAM_STR;
+        }
+        $this->statement->bindValue($parameter, $value, $type);
+    }
+
+    // Execute the prepare statement
+    public function execute() {
+        return $this->statement->execute();
+    }
+
+    // Return an Array
+    public function resultSet() {
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // Return a specific row as an object
+    public function single() {
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    // Gets the row count
+    public function rowCount() {
+        return $this->statement->rowCount();
+    }
+
 }
