26abefb33e9c246e7674c2209e941edd69d5e58e
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-10-23 14:47:23 +0600

    completed setup config.php file

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database params
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+// APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URLROOT (Dynamic Links)
+define('URLROOT', 'localhost/phpmvc');
+
+// Site name
+define('SITENAME', 'MVC Framework');
